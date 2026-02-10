9fa8062aaccd8147f0e120854d6b6780af856f63
FireTime <firetime@firetime.me>
2026-02-09 20:45:56 -0500

    added backrounds and stuff

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,11 @@
-import { Column, Heading, Text, AutoScroll, Logo, Fade } from "@once-ui-system/core";
+import {
+  Column,
+  Heading,
+  Text,
+  AutoScroll,
+  Logo,
+  Fade,
+} from "@once-ui-system/core";
 
 export default function NotFound() {
   return (
@@ -9,18 +16,66 @@ export default function NotFound() {
       <Heading marginBottom="l" variant="display-default-xs">
         Page Not Found
       </Heading>
-      <Text onBackground="neutral-weak">The page you are looking for does not exist.</Text>
-        <>
-          <Fade zIndex="1" to="right" fillHeight width="64" position="absolute" left="0" top="0"/>
-          <AutoScroll speed="fast" paddingY="40">
-                <Logo wordmark="/trademarks/wordmark-light.svg" size="xxl" href="https://firetime.me" dark/>
-                <Logo wordmark="/trademarks/wordmark-light.svg" size="xxl" href="https://firetime.me" light/>
-                <Logo wordmark="/trademarks/wordmark-light.svg" size="xxl" href="https://firetime.me" dark/>
-                <Logo wordmark="/trademarks/wordmark-light.svg" size="xxl" href="https://firetime.me" light/>
-                <Logo wordmark="/trademarks/wordmark-light.svg" size="xxl" href="https://firetime.me" dark/>
-                <Logo wordmark="/trademarks/wordmark-light.svg" size="xxl" href="https://firetime.me" light/>
-          </AutoScroll>
-        <Fade zIndex="1" to="right" fillHeight width="64" position="absolute" left="0" top="0"/>
+      <Text onBackground="neutral-weak">
+        The page you are looking for does not exist.
+      </Text>
+      <>
+        <Fade
+          Index="1"
+          to="right"
+          fillHeight
+          width="64"
+          position="absolute"
+          left="0"
+          top="0"
+        />
+        <AutoScroll speed="fast" paddingY="40">
+          <Logo
+            wordmark="/trademarks/wordmark-light.svg"
+            size="xxl"
+            href="https://firetime.me"
+            dark
+          />
+          <Logo
+            wordmark="/trademarks/wordmark-light.svg"
+            size="xxl"
+            href="https://firetime.me"
+            light
+          />
+          <Logo
+            wordmark="/trademarks/wordmark-light.svg"
+            size="xxl"
+            href="https://firetime.me"
+            dark
+          />
+          <Logo
+            wordmark="/trademarks/wordmark-light.svg"
+            size="xxl"
+            href="https://firetime.me"
+            light
+          />
+          <Logo
+            wordmark="/trademarks/wordmark-light.svg"
+            size="xxl"
+            href="https://firetime.me"
+            dark
+          />
+          <Logo
+            wordmark="/trademarks/wordmark-light.svg"
+            size="xxl"
+            href="https://firetime.me"
+            light
+          />
+        </AutoScroll>
+        <Fade
+          zIndex="1"
+          to="right"
+          fillHeight
+          width="64"
+          position="absolute"
+          left="0"
+          top="0"
+        />
       </>
     </Column>
   );
